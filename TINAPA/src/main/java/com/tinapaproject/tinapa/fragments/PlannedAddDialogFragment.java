3de3ad7b3f6efdd0fdfa82e85fa10ad334792805
DJ0Hybrid <dj0hybrid@gmail.com>
2015-08-20 10:10:42 -0500

    Species is now being filled in for the add planned dialog.

--- TINAPA/src/main/java/com/tinapaproject/tinapa/fragments/PlannedAddDialogFragment.java
+++ TINAPA/src/main/java/com/tinapaproject/tinapa/fragments/PlannedAddDialogFragment.java
@@ -1,14 +1,22 @@
 package com.tinapaproject.tinapa.fragments;
 
 import android.app.DialogFragment;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
 
 import com.tinapaproject.tinapa.R;
+import com.tinapaproject.tinapa.database.key.DexKeyValues;
+import com.tinapaproject.tinapa.database.provider.TinapaContentProvider;
 
 public class PlannedAddDialogFragment extends DialogFragment {
+
+    private Spinner mSpeciesSpinner;
 
     public static final String TAG = "PlannedAddDialogFragment";
 
@@ -26,6 +34,15 @@ public class PlannedAddDialogFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_planned_add_dialog, container, false);
+        View view = inflater.inflate(R.layout.fragment_planned_add_dialog, container, false);
+        mSpeciesSpinner = (Spinner) view.findViewById(R.id.planned_add_species_spinner);
+
+        Cursor speciesCursor = getActivity().getContentResolver().query(TinapaContentProvider.POKEDEX_ALL_SHORT_URI, null, null, null, null);
+        String[] from = {DexKeyValues.name};
+        int[] to = {R.id.simple_cell_name};
+        CursorAdapter mSpeciesCursorAdapter = new SimpleCursorAdapter(getActivity(), R.layout.cell_simple_name, speciesCursor, from, to, 0);
+        mSpeciesSpinner.setAdapter(mSpeciesCursorAdapter);
+
+        return view;
     }
 }
